9b629387da3f4307ef644ab949c2cfed49d74be9
Aviram <aviram@orbs.com>
2020-10-15 13:49:17 +0300

    finalize deployment script

--- contract-deployment/finalize-deployment.ts
+++ contract-deployment/finalize-deployment.ts
@@ -24,16 +24,16 @@ async function finalizeDeployment() {
     const initManager = accounts[0];
 
     const contractRegistry = web3.getExisting('ContractRegistry', contractRegistryAddress);
-    const electionsContract = web3.getExisting('Elections', await contractRegistry.getContract('elections') as any);
+    // const electionsContract = web3.getExisting('Elections', await contractRegistry.getContract('elections') as any);
     const stakingRewardsContract = web3.getExisting('StakingRewards', await contractRegistry.getContract('stakingRewards') as any);
     const feesAndBootstrapRewardsContract = web3.getExisting('FeesAndBootstrapRewards', await contractRegistry.getContract('feesAndBootstrapRewards') as any);
-    const committeeContract = web3.getExisting('Committee', await contractRegistry.getContract('committee') as any);
-    const previousCommitteeContract = web3.getExisting('Committee', config.previousCommitteeContractAddress);
+    // const committeeContract = web3.getExisting('Committee', await contractRegistry.getContract('committee') as any);
+    // const previousCommitteeContract = web3.getExisting('Committee', config.previousCommitteeContractAddress);
 
-    const committee = (await previousCommitteeContract.getCommittee())[0];
+    // const committee = (await previousCommitteeContract.getCommittee())[0];
     // await committeeContract.unlock({from: initManager});
-    await electionsContract.initReadyForCommittee(committee, {from: initManager});
-    await committeeContract.emitCommitteeSnapshot();
+    // await electionsContract.initReadyForCommittee(committee, {from: initManager});
+    // await committeeContract.emitCommitteeSnapshot();
 
     await feesAndBootstrapRewardsContract.activateRewardDistribution(await web3.now(), {from: initManager});
     await stakingRewardsContract.activateRewardDistribution(await web3.now(), {from: initManager});
