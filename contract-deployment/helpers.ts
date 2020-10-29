640e612cc8c2a47de53e20d31250a04e349406e4
Aviram <aviram@orbs.com>
2020-10-29 08:43:23 +0200

    rewards upgrade and migration

--- contract-deployment/helpers.ts
+++ contract-deployment/helpers.ts
@@ -1,3 +1,7 @@
 import BN from "bn.js";
 
+export const DEPLOYMENT_SUBSET_MAIN = "main";
+export const DEPLOYMENT_SUBSET_CANARY = "canary";
+export const ZERO_ADDR = "0x0000000000000000000000000000000000000000";
+
 export const bn = x => new BN(x);
